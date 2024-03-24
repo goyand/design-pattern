653b668de072168b8436cfb6c888262ecca42db3
goyand <sennkoub@gmail.com>
2024-03-24 09:26:06 +0900

    add producer consumer - exchange

---
+++ src/main/java/jp/goyand/concurrency/exchange/ProducerThread.java
@@ -0,0 +1,40 @@
+package jp.goyand.concurrency.exchange;
+
+import java.util.Random;
+import java.util.concurrent.Exchanger;
+
+public class ProducerThread extends Thread {
+    private final Exchanger<char[]> exchanger;
+    private char[] buffer;
+    private char index = 0;
+    private final Random random;
+
+    public ProducerThread(Exchanger<char[]> exchanger, char[] buffer, long seed) {
+        super("ProducerThread");
+        this.exchanger = exchanger;
+        this.buffer = buffer;
+        this.random = new Random(seed);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                for (int i = 0; i < buffer.length; i++) {
+                    buffer[i] = nextChar();
+                    System.out.println(Thread.currentThread().getName() + " -> " + buffer[i]);
+                }
+                buffer = exchanger.exchange(buffer);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private char nextChar() throws InterruptedException {
+        char c = (char)('A' + index % 26);
+        index++;
+        Thread.sleep(random.nextInt(1000));
+        return c;
+    }
+}
--- src/main/java/jp/goyand/concurrency/producerconsumer/Table.java
+++ src/main/java/jp/goyand/concurrency/producerconsumer/Table.java
@@ -1,34 +1,55 @@
 package jp.goyand.concurrency.producerconsumer;
 
-public class Table {
-    private final String[] buffer;
-    private int tail;
-    private int head;
-    private int count;
+import java.util.concurrent.ArrayBlockingQueue;
+
+// Example 1
+//public class Table {
+//    private final String[] buffer;
+//    private int tail;
+//    private int head;
+//    private int count;
+//    public Table(int count) {
+//        this.buffer = new String[count];
+//        this.head = 0;
+//        this.tail = 0;
+//        this.count = 0;
+//    }
+//    public synchronized void put(String cake) throws InterruptedException {
+//        System.out.println(Thread.currentThread().getName() + " puts " + cake);
+//        while (count >= buffer.length) {
+//            wait();
+//        }
+//        buffer[tail] = cake;
+//        tail = (tail + 1) % buffer.length;
+//        count++;
+//        notifyAll();
+//    }
+//    public synchronized String take() throws InterruptedException {
+//        while (count <= 0) {
+//            wait();
+//        }
+//        String cake = buffer[head];
+//        head = (head + 1) % buffer.length;
+//        count--;
+//        notifyAll();
+//        System.out.println(Thread.currentThread().getName() + " takes " + cake);
+//        return cake;
+//    }
+//}
+
+// Example 2
+public class Table extends ArrayBlockingQueue<String> {
     public Table(int count) {
-        this.buffer = new String[count];
-        this.head = 0;
-        this.tail = 0;
-        this.count = 0;
+        super(count);
     }
-    public synchronized void put(String cake) throws InterruptedException {
+
+    public void put(String cake) throws InterruptedException {
         System.out.println(Thread.currentThread().getName() + " puts " + cake);
-        while (count >= buffer.length) {
-            wait();
-        }
-        buffer[tail] = cake;
-        tail = (tail + 1) % buffer.length;
-        count++;
-        notifyAll();
+        super.put(cake);
     }
-    public synchronized String take() throws InterruptedException {
-        while (count <= 0) {
-            wait();
-        }
-        String cake = buffer[head];
-        head = (head + 1) % buffer.length;
-        count--;
-        notifyAll();
+
+    public String take() throws InterruptedException {
+        String cake = super.take();
         System.out.println(Thread.currentThread().getName() + " takes " + cake);
         return cake;
     }
